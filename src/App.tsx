db56b2ced592dfd908dfefd0fc6dcb1bff7bba1f
Lucas Vitorino <lucas.vitorino@alterans.tech>
2022-10-23 20:04:52 -0300

    feat(*): transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
+import { createServer } from "miragejs";
+
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
-import {Dashboard} from "./components/Dashboard";
+import { Dashboard } from "./components/Dashboard";
+import Modal from "react-modal";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", (req, res) => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          type: "deposit",
+          category: "food",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
+
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  const handleOpenNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(true);
+  };
+
+  const handleCloseNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(false);
+  };
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
